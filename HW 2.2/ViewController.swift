db05be2210fd01a96dc8433b81eed1b00e37566b
Zhenya <Henry_blackburn@icloud.com>
2024-03-23 20:46:13 +0400

    Initial Commit

---
+++ HW 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.2
+//
+//  Created by Евгений on 23.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
