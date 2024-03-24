5de48f959ae801dd4faf6da2a86d204a233be66e
Zhenya <Henry_blackburn@icloud.com>
2024-03-24 20:39:11 +0400

    result

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var button: UIButton!
+    
+    private var light = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        button.layer.cornerRadius = 10
+        
+        redLight.layer.cornerRadius = 50
+        yellowLight.layer.cornerRadius = 50
+        greenLight.layer.cornerRadius = 50
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
     }
 
-
+    @IBAction func butonPressed() {
+        
+        if button.currentTitle == "Включить" {
+            button.setTitle("Следующий", for: .normal)
+        }
+        
+        if light == 1 {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            light = 2
+        } else if light == 2 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            light = 3
+        } else if light == 3 {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            light = 1
+        }
+    }
 }
 
